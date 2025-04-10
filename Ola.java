b6eefcc23f836b57e9ef8716486ef5cf33c306f9
seffany-oliv <steffany.oliv1@gmail.com>
2025-04-10 16:36:56 -0300

    Add UI

--- Ola.java
+++ Ola.java
@@ -1,5 +1,8 @@
+import javax.swing.JOptionPane;
+
 public class Ola{
     public static void main(String[] args){
-        System.out.println("Boa tarde á todos!");
+        //System.out.println("Boa tarde á todos!");
+        JOptionPane.showMessageDialog(null, "Boa tarde á todos!");
     }
 }
